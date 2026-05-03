2ab15e12bc34dce084a31ea3f270b00df076d2df
Henri Guillot <henri.guihen01@gmail.com>
2026-05-03 18:50:51 -0400

    clean repo without terraform artifacts

---
+++ terraform/envs/prod/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+  cluster_name    = "simple-platform"
+  cluster_version = "1.29"
+
+  vpc_id     = "vpc-0518bdeefb96a886d"
+  subnet_ids = ["subnet-04ab7f22ddced88f9", "subnet-07b548eec9ff23574"]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 3
+    }
+  }
+}
